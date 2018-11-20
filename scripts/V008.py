d005425f268560d19d5d7de7ecc4c8697237f88d
André Brandão <andrelbd1@gmail.com>
2018-11-20 14:46:19 -0200

    Adding heatmap to view students' access by week

--- scripts/V008.py
+++ scripts/V008.py
@@ -164,14 +164,14 @@ class V008:
 
     # Heatmap
     def graph_04(self):
-        legend = {"title":"Número de acessos por estudante",
+        legend = {"title":"Número de acessos dos estudantes por dia",
                     "xaxis":"",
                     "yaxis":"",
                     "columns":{1:"Dom", 2:"Seg", 3:"Ter", 4:"Qua", 5:"Qui", 6:"Sex", 0:"Sáb"},
                     "misc":{1:"dia", 2:"Trabalho", 3:"Prova"},
                 }
         if (self._language == "en"):
-            legend = {"title":"Number of access by student",
+            legend = {"title":"Number of students' access by day",
                         "xaxis":"",
                         "yaxis":"",
                         "columns":{1:"Sun", 2:"Mon", 3:"Tue", 4:"Wed", 5:"Thu", 6:"Fri", 0:"Sat"},
@@ -229,14 +229,14 @@ class V008:
         iplot(fig, filename='Heatmap')
 
     def graph_05(self):
-        legend = {"title":"Número de acessos por estudante",
+        legend = {"title":"Número de acessos dos estudantes por dia",
                     "xaxis":"",
                     "yaxis":"",
                     "columns":{1:"Dom", 2:"Seg", 3:"Ter", 4:"Qua", 5:"Qui", 6:"Sex", 0:"Sáb"},
                     "misc":{1:"dia", 2:"Trabalho", 3:"Prova"},
                 }
         if (self._language == "en"):
-            legend = {"title":"Number of access by student",
+            legend = {"title":"Number of students' access by day",
                         "xaxis":"",
                         "yaxis":"",
                         "columns":{1:"Sun", 2:"Mon", 3:"Tue", 4:"Wed", 5:"Thu", 6:"Fri", 0:"Sat"},
@@ -317,6 +317,224 @@ class V008:
         fig = Figure(data=data, layout=layout)
         iplot(fig, filename='Heatmap')
 
+    def graph_06(self):
+        legend = {"title":"Número de acessos dos estudantes por semana",
+                    "xaxis":"",
+                    "yaxis":"",
+                    "columns":'semana',
+                    "misc":{1:"Trabalho", 2:"Prova"},
+                }
+        if (self._language == "en"):
+            legend = {"title":"Number of students' access by week",
+                        "xaxis":"",
+                        "yaxis":"",
+                        "columns":"semana",
+                        "misc":{1:"Work", 2:"Test"},
+                    }
+        df = self._df_sum_week
+        z = []
+        for i in range (1, len(df.columns)):
+            z.append(df.iloc[:,i].values.tolist())        
+        
+        lst = [legend["columns"]+" "+str(i+1) for i in range (0,len(df.columns[1:]))]
+        lst[int(self._work_deadline/7)-1] = "<b>"+legend["columns"]+" "+str(int(self._work_deadline/7))+",<br>"+legend["misc"][1]+"</b>"
+        lst[int(self._test_day/7)-1] = "<b>"+legend["columns"]+" "+str(int(self._test_day/7))+",<br>"+legend["misc"][2]+"</b>"
+        
+        trace = Heatmap(z=z, #Access
+                        y=lst, #Weeks
+                        x=df.iloc[:,0], #Students
+                        colorscale=[[0, 'rgb(255,255,255)'], [1, 'rgb(0,0,255)']],
+                        showscale = True
+                    )
+        
+        layout = Layout(
+                title = legend["title"],
+                autosize=True,
+                # width=1050,
+                # height=950,
+                hovermode = "closest",                
+                xaxis=dict(
+                    title = legend["xaxis"],
+                    titlefont=dict(
+                        # family='Arial, sans-serif',
+                        # size=18,
+                        color='rgb(180,180,180)',
+                    ),
+                ),
+                yaxis=dict(
+                    title = legend["yaxis"],
+                    titlefont=dict(
+                        # family='Arial, sans-serif',
+                        # size=18,
+                        color='rgb(180,180,180)',
+                    ),
+                    showticklabels=True,
+                    type="category",
+                    tick0=0,
+                    dtick=1,
+                    exponentformat='e',
+                    showexponent='all',
+                    gridcolor='#bdbdbd',
+                )
+            )
+
+        data = [trace]
+        fig = Figure(data=data, layout=layout)
+        iplot(fig, filename='Heatmap')
+
+    def graph_07(self):
+        legend = {"title":"Número de acessos dos estudantes por semana",
+                    "xaxis":"",
+                    "yaxis":"",
+                    "columns":'semana',
+                    "misc":{1:"Trabalho", 2:"Prova"},
+                }
+        if (self._language == "en"):
+            legend = {"title":"Number of students' access by week",
+                        "xaxis":"",
+                        "yaxis":"",
+                        "columns":"semana",
+                        "misc":{1:"Work", 2:"Test"},
+                    }
+        df = self._df_sum_week
+        z = []
+        max_value = 0
+        for i in range (1, len(df.columns)):
+            z.append(df.iloc[:,i].values.tolist())
+            max_local = max(df.iloc[:,i].values.tolist())
+            max_value = max(max_local,max_value)
+        
+        lst = [legend["columns"]+" "+str(i+1) for i in range (0,len(df.columns[1:]))]
+        lst[int(self._work_deadline/7)-1] = "<b>"+legend["columns"]+" "+str(int(self._work_deadline/7))+",<br>"+legend["misc"][1]+"</b>"
+        lst[int(self._test_day/7)-1] = "<b>"+legend["columns"]+" "+str(int(self._test_day/7))+",<br>"+legend["misc"][2]+"</b>"
+        
+        trace = Heatmap(z=z, #Access
+                        y=lst, #Weeks
+                        x=df.iloc[:,0], #Students
+                        colorscale=[[0, 'rgb(255,255,255)'], [1, 'rgb(0,0,255)']],
+                        showscale = True
+                    )
+        
+        annotations=[]
+        for i in range(1,len(df.columns)):
+            for j in range(0,len(df)):
+                color = 'rgb(0,0,0)'
+                if df.iloc[j,i] > max_value/2:
+                    color = 'rgb(255,255,255)'
+                annotations.append({
+                    "text":str(df.iloc[j,i]),
+                    "y":lst[i-1],
+                    "x":df.iloc[j,0],
+                    "xref":'x1', 
+                    "yref":'y1',
+                    "showarrow":False,
+                    "font":{
+                        # "family":'Courier New, monospace',
+                        "size":9,
+                        "color":color
+                    }
+                })
+
+        layout = Layout(
+                title = legend["title"],
+                autosize=True,
+                # width=1050,
+                # height=950,
+                hovermode = "closest",                
+                xaxis=dict(
+                    title = legend["xaxis"],
+                    titlefont=dict(
+                        # family='Arial, sans-serif',
+                        # size=18,
+                        color='rgb(180,180,180)',
+                    ),
+                ),
+                yaxis=dict(
+                    title = legend["yaxis"],
+                    titlefont=dict(
+                        # family='Arial, sans-serif',
+                        # size=18,
+                        color='rgb(180,180,180)',
+                    ),
+                    showticklabels=True,
+                    type="category",
+                    tick0=0,
+                    dtick=1,
+                    exponentformat='e',
+                    showexponent='all',
+                    gridcolor='#bdbdbd',
+                ),
+                annotations=annotations
+            )
+
+        data = [trace]
+        fig = Figure(data=data, layout=layout)
+        iplot(fig, filename='Heatmap')
+
+    # def graph_08(self):
+    #     legend = {"title":"Variação de notas dos estudantes por cluster",
+    #                 "xaxis":"",
+    #                 "yaxis":"Notas",
+    #             }
+    #     if (self._language == "en"):
+    #         legend = {"title":"Students' grades variation by cluster",
+    #                     "xaxis":"",
+    #                     "yaxis":"Grades",
+    #                 }
+    #     df = self._df_all_day
+    #     Clusters = df.Cluster.unique()
+    #     color = ["rgb(255,0,0)","rgb(127,0,127)","rgb(0,0,255)","rgb(0,255,0)"]        
+    #     # print(Clusters)
+    #     trace = []
+    #     for i in range(0,len(Clusters)):
+    #         trace.append(
+    #             Box(
+    #                 y=df.Grade.loc[df['Cluster']==Clusters[i]].values.tolist(), #Access
+    #                 name="Cluster "+str(i+1),
+    #                 text=df["Students"].loc[df['Cluster']==Clusters[i]].values.tolist(),
+    #                 boxpoints = 'all',
+    #                 marker=dict(
+    #                     color = color[i],
+    #                     line=dict(
+    #                         width=1
+    #                     )                        
+    #                 ),
+    #                 boxmean=True
+    #             )
+    #         )
+
+    #     layout = Layout(
+    #         title=legend['title'],
+    #         # hovermode = "closest",
+    #         showlegend = True,
+    #         xaxis = dict(
+    #             title = legend["xaxis"],
+    #             titlefont=dict(
+    #                 # family='Arial, sans-serif',
+    #                 # size=18,
+    #                 color='rgb(180,180,180)',
+    #             ),
+    #         ),
+    #         yaxis = dict(
+    #             title = legend["yaxis"],
+    #             titlefont=dict(
+    #                 # family='Arial, sans-serif',
+    #                 # size=18,
+    #                 color='rgb(180,180,180)',
+    #             ),
+    #             fixedrange = False,
+    #             range = [-1, self.DATASET.Grade.max()+10],
+    #             rangemode = "normal",
+    #             # showline = True,                
+    #             zeroline = False,
+    #         )
+    #     )
+
+    #     data = trace
+    #     fig = Figure(data=data, layout=layout)
+    #     iplot(fig, filename='box-plot')
+
+
     def print_all_graphs(self,language="pt"):
         self._language = language
         self.graph_01() #Table raw
@@ -324,9 +542,9 @@ class V008:
         self.graph_03()
         self.graph_04() # Heatmap
         self.graph_05()
-        # self.graph_06() #Box
-        # self.graph_07()
-        # self.graph_08()
+        self.graph_06() 
+        self.graph_07()
+        # self.graph_08() #Box
         # self.graph_09()
         # self.graph_10() #Violin
         # self.graph_11()
@@ -334,5 +552,5 @@ class V008:
         # self.graph_13()
 
 instance = V008(number_students=35, number_weeks=7)
-instance.print_all_graphs("pt")
-# instance.print_all_graphs("en")
+# instance.print_all_graphs("pt")
+instance.print_all_graphs("en")
